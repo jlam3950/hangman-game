ffd8a08fa5b88b06115b0ba6b25e4269ad4dd36d
Chris Nguyen <chrishiepnguyen@yahoo.com>
2022-03-22 16:21:30 -0500

    adds new files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let a = 20;
-let b = 10;
-let c = a + b
+// let a = 20;
+// let b = 10;
+// let c = a + b
 
-for (let a = 0; a<=15; a++){
-    if(a%2==0){
-        console.log(a);
-    }
-}
+// for (let a = 0; a<=15; a++){
+//     if(a%2==0){
+//         console.log(a);
+//     }
+// }
+
