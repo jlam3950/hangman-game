3fc5d6d77ede2192db48e4817ed15303f7bff5f5
Rachel Parris <rachel.parris@gmail.com>
2022-04-23 12:51:05 -0400

    Fix guesses functionality.

--- hangman.js
+++ hangman.js
@@ -9,6 +9,14 @@ window.onload = function (){
     document.addEventListener("keydown", (event) => {
         // Needs to check if a guess is incorrect. If so the counter will decrease by 1 and render that number to the page.
         counter = counter - 1;
+
+        if (counter <= -1) {
+            return;
+        } else if (counter === 0) {
+            guessesId.innerHTML = `<h2>You have ${counter} lives. Sorry you lost the game.</h2>`;
+            return;
+        }
+
         guessesId.innerHTML = `<h2>You have ${counter} lives.</h2>`;
         console.log(event.key);
         console.log(counter);
